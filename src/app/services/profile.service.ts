6d0de0fdc9fc7bacd21be52f351f6b48e3fec7ca
Michael-mbogo <mikembogo17@gmail.com>
2018-06-05 19:27:12 +0300

    Add exports

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -7,5 +7,15 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ProfileService {
 
+  private username:string;
+
+  private clientid = '2ddb51df13183ba55b6d';
+  private clientsecret= '2462f0401923eb74b1d737bfbbcd24ea8454430f';
+
+  constructor(private http:HttpClient) {
+    console.log("Service is now ready!");
+    this.username= 'Michael-mbogo';
+}
+
   constructor() { }
 }
